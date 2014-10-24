241d623da9517a6658eadb90ea17fe056b326c2f
Dima Valuev <document.letter@gmail.com>
2014-10-24 12:04:18 +0400

    Remove copy jade files & add layout jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,3 @@
-/**
- *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-
 'use strict';
 
 // Include Gulp & Tools We'll Use
@@ -39,6 +20,7 @@ var AUTOPREFIXER_BROWSERS = [
       'Safari >= 6'
 ];
 
+// Jade 
 gulp.task('jade', function() {
   return gulp.src('app/*.jade')
     .pipe($.jade({
@@ -72,7 +54,8 @@ gulp.task('images', function () {
 gulp.task('copy', function () {
   return gulp.src([
     'app/*',
-    '!app/*.html',
+    '!app/*.jade',
+    '!app/layout',
     'node_modules/apache-server-configs/dist/.htaccess'
   ], {
     dot: true
